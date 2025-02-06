e734af65cc8f257e3047accf6694a016da242e95
lukesilva.dev@gmail.com <lukelimasilva@outlook.com>
2025-02-05 22:41:29 -0300

    fix: fix init.sql

--- init.sql
+++ init.sql
@@ -1,14 +1,22 @@
 DO $$
 BEGIN
-    IF NOT EXISTS (SELECT 1 FROM pg_tables WHERE tablename = 'product') THEN
-        CREATE TABLE product(
+    IF NOT EXISTS (
+        SELECT 1 
+        FROM information_schema.tables 
+        WHERE table_schema = 'public' 
+        AND table_name = 'product'
+    ) THEN
+        EXECUTE 'CREATE TABLE product (
             id SERIAL PRIMARY KEY,
             product_name VARCHAR(50) NOT NULL,
             price NUMERIC(10, 2) NOT NULL
-        );
-        
+        )';
+    END IF;
+
+    IF NOT EXISTS (
+        SELECT 1 FROM product WHERE product_name = 'Sushi'
+    ) THEN
         INSERT INTO product(product_name, price)
-        SELECT 'Sushi', 100
-        WHERE NOT EXISTS (SELECT 1 FROM product WHERE product_name = 'Sushi');
+        VALUES ('Sushi', 100);
     END IF;
 END $$;
